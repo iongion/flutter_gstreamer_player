5cc3f93d2593e2f63893be232c464c9ce79e0d9f
e.morvan <e.morvan@inanix.fr>
2023-09-26 15:23:14 +0200

    Update flutter_gstreamer_player.dart

--- lib/flutter_gstreamer_player.dart
+++ lib/flutter_gstreamer_player.dart
@@ -1,8 +1,6 @@
-// ignore_for_file: unnecessary_null_comparison, must_be_immutable
+// ignore_for_file: unnecessary_null_comparison, must_be_immutable, avoid_print
 
 import 'dart:async';
-import 'dart:io';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 // import 'package:flutter_gif/flutter_gif.dart';
@@ -10,8 +8,8 @@ import 'package:flutter/material.dart';
 class GstPlayerTextureController {
   static const MethodChannel _channel =
       MethodChannel('flutter_gstreamer_player');
-  int textureId = 0;
 
+  int textureId = 0;
   static const int _id = 0;
 
   Future<int> initialize(String pipeline) async {
@@ -53,16 +51,10 @@ class GstPlayer extends StatefulWidget {
 class _GstPlayerState extends State<GstPlayer> with TickerProviderStateMixin {
   final _controller = GstPlayerTextureController();
   GlobalKey key = GlobalKey();
-  bool isReceive = true;
-  // late FlutterGifController controllerGif;
   @override
   void initState() {
     super.initState();
     initializeController();
-    // udpreceive();
-    // controllerGif = FlutterGifController(vsync: this);
-    // controllerGif.repeat(
-    //     min: 0, max: 29, period: const Duration(milliseconds: 2500));
   }
 
   @override
@@ -76,32 +68,9 @@ class _GstPlayerState extends State<GstPlayer> with TickerProviderStateMixin {
 
   Future<void> initializeController() async {
     await _controller.initialize(widget.pipeline);
-    if (Platform.isAndroid) {
-      await _controller.versionAndroid();
-    }
     print("initializeController");
     setState(() {});
   }
-
-  //     final udpSocket =
-  //         await RawDatagramSocket.bind(InternetAddress.anyIPv4, udpPort);
-  //     udpSocket.listen((RawSocketEvent event) {
-  //       if (event == RawSocketEvent.read) {
-  //         final datagram = udpSocket.receive();
-  //         if (datagram != null) {
-  //           print(
-  //               'Données UDP reçues depuis ${datagram.address}:${datagram.port}');
-  //           isReceive = true;
-  //           setState(() {});
-  //         }
-  //       }
-  //     });
-  //     isReceive = false;
-  //     print('En attente de données UDP sur le port $udpPort...');
-  //     setState(() {});
-  //     await Future.delayed(const Duration(seconds: 1));
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -111,30 +80,20 @@ class _GstPlayerState extends State<GstPlayer> with TickerProviderStateMixin {
       case TargetPlatform.linux:
       case TargetPlatform.android:
         return Container(
-            child: isReceive
-                ? Texture(key: key, textureId: _controller.textureId)
-                : Container(
-                    color: Colors.amber,
-                  )
-            // : GifImage(
-            //     width: double.infinity,
-            //     height: double.infinity,
-            //     repeat: ImageRepeat.repeat,
-            //     controller: controllerGif,
-            //     image: const AssetImage("assets/images/neige.gif"),
-            //   ),
-            );
-
+          child: _controller.isInitialized
+              ? Texture(textureId: _controller.textureId)
+              : null,
+        );
       case TargetPlatform.iOS:
-        print("print gstreamer ios");
         String viewType = _controller.textureId.toString();
         final Map<String, dynamic> creationParams = <String, dynamic>{};
+        print("gstreamer ios viewType: $viewType , ");
         return UiKitView(
           key: key,
           viewType: viewType,
           layoutDirection: TextDirection.ltr,
           creationParams: creationParams,
-          creationParamsCodec: StandardMessageCodec(),
+          creationParamsCodec: const StandardMessageCodec(),
         );
 
       default:
