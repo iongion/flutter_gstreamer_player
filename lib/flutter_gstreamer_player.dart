1ac57d22e1ee21c12fc66daf9f15a8ba6a19c0a6
e.morvan <e.morvan@inanix.fr>
2023-10-11 15:27:03 +0200

    Suppression de l'incrémentation de l'id texture

--- lib/flutter_gstreamer_player.dart
+++ lib/flutter_gstreamer_player.dart
@@ -16,10 +16,10 @@ class GstPlayerTextureController {
   Future<int> initialize(String pipeline) async {
     // No idea why, but you have to increase `_id` first before pass it to method channel,
     // if not, receiver of method channel always received 0
-    if (currentPlatform == "ios") {
-      GstPlayerTextureController._id = GstPlayerTextureController._id + 1;
-      print("GstPlayerTextureController.id +1");
-    }
+    // if (currentPlatform == "ios") {
+    // GstPlayerTextureController._id = GstPlayerTextureController._id + 1;
+    print("GstPlayerTextureController.id");
+    // }
     textureId = await _channel.invokeMethod('PlayerRegisterTexture', {
       'pipeline': pipeline,
       'playerId': GstPlayerTextureController._id,
